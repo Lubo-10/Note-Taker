e638913862727883f3a04a6e41c80a1aa8e94695
Lubo-10 <bernlubo01@gmail.com>
2022-07-28 17:35:37 -0400

    get started

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const PORT= process.env.PORT || 3001
-const apiRoutes = require('./routes/apiroutes/notesRoutes');
-const htmlRoutes = require('./routes/htmlRoutes/htmlRoutes');
+const PORT= 3005;
+// const apiRoutes = require('./routes/api');
+// const htmlRoutes = require('./routes/html');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+})
 
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`)
+    console.log(`Server is running on port http://localhost:${PORT}/`)
 })
